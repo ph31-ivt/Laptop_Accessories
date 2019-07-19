063ce9200d13bbcdbd738174d46cac7737e09221
Thindang <thinhdanghuu94@gmail.com>
2019-07-19 17:50:59 +0700

    text

---
+++ app/ProductPromtion.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class ProductPromtion extends Model
+{
+    use SoftDeletes;
+    protected $productpromotions=['deleted_at'];
+    protected $table = 'product_promotion';
+    protected $fillable =[
+    	'product_id', 'promotion_id'
+    ];
+
+    // test
+}
